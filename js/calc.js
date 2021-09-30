7c37410a48e492d804f4f22fc49756e7a8bc4ffd
Matt Rudge <matt.rudge@codeinstitute.net>
2021-09-30 15:27:06 +0000

    Add config for factors

--- js/calc.js
+++ js/calc.js
@@ -1,17 +1,23 @@
 let optima = {
-    "o": 3,
+    "o": 3.0,
     "p": 1.2,
-    "t": 1,
-    "i": 11,
-    "m": 1,
-    "a": 8
+    "t": 1.0,
+    "i": 11.0,
+    "m": 1.0,
+    "a": 8.0,
+    "wpm": 120
 }
+
+let configModal = new bootstrap.Modal(document.getElementById("configModal"));
 
 let fields = document.getElementsByClassName("form-control");
 
 for (let field of fields) {
     field.addEventListener("change", recalculate);
 }
+
+document.getElementById("config").addEventListener("click", show);
+document.getElementById("savebutton").addEventListener("click", save);
 
 function recalculate() {
 
@@ -33,7 +39,7 @@ function recalculate() {
     let running_total = 0;
     running_total += o_num * o_len * optima.o;
     running_total += p_num * p_len * optima.p;
-    running_total += (t_num * (t_len / 120) * optima.t);
+    running_total += (t_num * (t_len / optima.wpm) * optima.t);
     running_total += i_num * i_len * optima.i;
     running_total += m_num * m_len * optima.m;
     running_total += a_num * a_len * optima.a;
@@ -41,4 +47,26 @@ function recalculate() {
 
     document.getElementById("totaltime").innerText = (running_total / 60).toFixed(2) + " hours";
 
+}
+
+function show() {
+    document.getElementById("o").value = optima.o;
+    document.getElementById("p").value = optima.p;
+    document.getElementById("t").value = optima.t;
+    document.getElementById("i").value = optima.i;
+    document.getElementById("m").value = optima.m;
+    document.getElementById("a").value = optima.a;
+    document.getElementById("wpm").value = optima.wpm;
+    configModal.show();
+}
+
+function save() {
+    optima.o = parseFloat(document.getElementById("o").value);
+    optima.p = parseFloat(document.getElementById("p").value);
+    optima.t = parseFloat(document.getElementById("t").value);
+    optima.i = parseFloat(document.getElementById("i").value);
+    optima.m = parseFloat(document.getElementById("m").value);
+    optima.a = parseFloat(document.getElementById("a").value);
+    optima.wpm = parseFloat(document.getElementById("wpm").value);
+    configModal.hide();
 }
